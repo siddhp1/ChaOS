aab5e5c8e624a86260af01db02e0857ca0fec9dc
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-01-23 14:06:48 -0500

    Add memory test

--- kernel/main.c
+++ kernel/main.c
@@ -3,26 +3,43 @@
 #include "kernel/cpu.h"
 #include "kernel/printk.h"
 #include "kernel/uart.h"
+#include "memory/heap.h"
+#include "memory/kmap.h"
+#include "memory/memory.h"
+#include "memory/page.h"
 
 extern uintptr_t bss_start;
 extern uintptr_t bss_end;
 
 void kernel_entry(void) {
   // Zero out the BSS (static variables)
-  volatile uintptr_t *ptr = (volatile uintptr_t*)&bss_start;
+  volatile uintptr_t* ptr = (volatile uintptr_t*)&bss_start;
   while (ptr < &bss_end) {
     *ptr++ = 0;
   }
 
+  // Phase one
   uart_init();
   exception_init();
 
   printk("Hello OS!\n");
 
   // Trigger a panic to test exception handling
-  volatile int *p = (int*)0xDEADBEEF;
-  volatile int x = *p;
-  (void)x;
+  // volatile int* p = (int*)0xDEADBEEF;
+  // volatile int x = *p;
+  // (void)x;
+
+  // Phase two
+  memory_init();
+
+  // Test allocating and freeing a page
+  struct page* page = alloc_page();
+  void* virtual = kmap(page);
+  uintptr_t* vptr = (uintptr_t*)virtual;
+  while ((uintptr_t)vptr < (uintptr_t)virtual + PAGE_SIZE) {
+    *vptr++ = 0xAB;
+  }
+  free_page(page);
 
   while (1) asm volatile("WFI");
 }
