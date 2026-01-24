647bc765f3a3ad741f8930ce12e472b111f2aa5d
Jenneva Li <jenli0508@gmail.com>
2026-01-24 17:52:34 -0500

    Fix formatting

--- drivers/uart/uart.c
+++ drivers/uart/uart.c
@@ -1,12 +1,11 @@
 #include "kernel/uart.h"
 
 #define UART0_BASE 0x09000000UL
-#define UART_DR   (*(volatile unsigned int*)(UART0_BASE + 0x00))
-#define UART_FR   (*(volatile unsigned int*)(UART0_BASE + 0x18))
-#define TXFF      (1 << 5)
+#define UART_DR (*(volatile unsigned int*)(UART0_BASE + 0x00))
+#define UART_FR (*(volatile unsigned int*)(UART0_BASE + 0x18))
+#define TXFF (1 << 5)
 
-void uart_init(void) {
-}
+void uart_init(void) {}
 
 void uart_putc(char c) {
   while (UART_FR & TXFF);
