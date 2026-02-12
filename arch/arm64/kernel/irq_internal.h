3822ff1842554020998e3ccca15359ec90798f8a
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-02-11 23:05:53 -0500

    Add IRQ handling

--- arch/arm64/kernel/irq_internal.h
+++ arch/arm64/kernel/irq_internal.h
@@ -1,6 +1,13 @@
 #ifndef IRQ_INTERNAL_H
 #define IRQ_INTERNAL_H
 
+#include <stdint.h>
+
+typedef void (*irq_handler_t)(void*);
+
+void irq_ack(uint32_t irq);
+uint32_t irq_get_pending(void);
 void irq_handler(void);
+void register_irq(int irq, irq_handler_t handler);
 
 #endif
