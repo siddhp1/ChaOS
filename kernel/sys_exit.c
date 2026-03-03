6368b45eb7439b1ff7b453335f87a4f9223593bc
Jenneva Li <jenli0508@gmail.com>
2026-03-03 00:51:22 -0500

    Update sys_exit for userspace

--- kernel/sys_exit.c
+++ kernel/sys_exit.c
@@ -1,10 +1,21 @@
 #include "kernel/scheduler.h"
+#include "kernel/task.h"
+#include "kernel/vm.h"
 
 long sys_exit(long status, long a1, long a2, long a3, long a4, long a5) {
-  // TOOD: Add process exit status
-  // current_task->exit_status = status;
+  (void)status;
+  (void)a1;
+  (void)a2;
+  (void)a3;
+  (void)a4;
+  (void)a5;
 
-  schedule();
+  if (current_task->ttbr0) {
+    vm_destroy_user(current_task);
+  }
 
-  while (1);  // Should never return
+  current_task->state = TASK_ZOMBIE;
+  need_schedule = true;
+
+  while (1) asm volatile("wfi");
 }
