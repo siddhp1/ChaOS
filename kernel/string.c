57791ee62adddef666dffbec80876faf03098454
Jenneva Li <jenli0508@gmail.com>
2026-03-03 00:47:04 -0500

    Extend string utilities

--- kernel/string.c
+++ kernel/string.c
@@ -11,3 +11,51 @@ void* memset(void* s, int c, size_t n) {
   }
   return s;
 }
+
+void* memcpy(void* dst, const void* src, size_t n) {
+  char* d = (char*)dst;
+  const char* s = (const char*)src;
+
+  for (size_t i = 0; i < n; i++) {
+    d[i] = s[i];
+  }
+  return dst;
+}
+
+size_t strlen(const char* s) {
+  size_t len = 0;
+  while (*s++) len++;
+  return len;
+}
+
+int strcmp(const char* a, const char* b) {
+  while (*a && *a == *b) {
+    a++;
+    b++;
+  }
+  return (unsigned char)*a - (unsigned char)*b;
+}
+
+int strncmp(const char* a, const char* b, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    if (a[i] != b[i]) return (unsigned char)a[i] - (unsigned char)b[i];
+    if (a[i] == '\0') return 0;
+  }
+  return 0;
+}
+
+char* strncpy(char* dst, const char* src, size_t n) {
+  size_t i = 0;
+
+  while (i < n && src[i] != '\0') {
+    dst[i] = src[i];
+    i++;
+  }
+
+  while (i < n) {
+    dst[i] = '\0';
+    i++;
+  }
+
+  return dst;
+}
