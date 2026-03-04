97b314aa61434232c3140c3b3e6920ff67ec7155
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-03-03 19:22:47 -0500

    Fix task exit handler

--- kernel/sys_exit.c
+++ kernel/sys_exit.c
@@ -1,10 +1,12 @@
 #include "kernel/scheduler.h"
+#include "kernel/task.h"
 
 long sys_exit(long status, long a1, long a2, long a3, long a4, long a5) {
-  // TOOD: Add process exit status
+  // TODO: Add process exit status
   // current_task->exit_status = status;
 
+  current_task->state = TASK_ZOMBIE;
   schedule();
 
-  while (1);  // Should never return
+  return 0;
 }
