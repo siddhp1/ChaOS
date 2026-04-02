0cdd3ba126c709e92668796d7c53001c6801f862
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-04-01 21:39:04 -0400

    Update fault handlers
    Co-authored-by: Jenneva Li <jenli0508@gmail.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -6,22 +6,27 @@
 #include "kernel/printk.h"
 
 void data_abort_handler(void) {
-  uintptr_t far;
-  uint64_t esr;
-  asm volatile("mrs %0, FAR_EL1" : "=r"(far));
-  asm volatile("mrs %0, ESR_EL1" : "=r"(esr));
+  uint64_t elr, esr, far, spsr;
 
-  printk("DATA ABORT!\n");
+  asm volatile("mrs %0, ELR_EL1" : "=r"(elr));
+  asm volatile("mrs %0, ESR_EL1" : "=r"(esr));
+  asm volatile("mrs %0, FAR_EL1" : "=r"(far));
+  asm volatile("mrs %0, SPSR_EL1" : "=r"(spsr));
+
+  printk("DATA ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx",
+         elr, esr, far, spsr);
   panic("Page fault detected");
 }
 
 void prefetch_abort_handler(void) {
-  uint64_t esr;
-  uintptr_t far;
+  uint64_t elr, esr, far, spsr;
 
+  asm volatile("mrs %0, ELR_EL1" : "=r"(elr));
   asm volatile("mrs %0, ESR_EL1" : "=r"(esr));
   asm volatile("mrs %0, FAR_EL1" : "=r"(far));
+  asm volatile("mrs %0, SPSR_EL1" : "=r"(spsr));
 
-  printk("PREFETCH ABORT!\n");
-  panic("Kernel instruction fetch fault");
+  printk("PREFETCH ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx",
+         elr, esr, far, spsr);
+  panic("Kernel instruction fetch fault detected");
 }
