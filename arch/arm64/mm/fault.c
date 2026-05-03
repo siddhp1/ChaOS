b2ffe81a694054fb759f84afb3071cf50a859e57
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-05-02 21:36:50 -0400

    Add newline character to abort messages

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -13,7 +13,7 @@ void data_abort_handler(void) {
   asm volatile("mrs %0, FAR_EL1" : "=r"(far));
   asm volatile("mrs %0, SPSR_EL1" : "=r"(spsr));
 
-  printk("DATA ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx",
+  printk("DATA ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx\n",
          elr, esr, far, spsr);
   panic("Page fault detected");
 }
@@ -26,7 +26,8 @@ void prefetch_abort_handler(void) {
   asm volatile("mrs %0, FAR_EL1" : "=r"(far));
   asm volatile("mrs %0, SPSR_EL1" : "=r"(spsr));
 
-  printk("PREFETCH ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx",
-         elr, esr, far, spsr);
+  printk(
+      "PREFETCH ABORT!\nELR_EL1=%lx\nESR_EL1=%lx\nFAR_EL1=%lx\nSPSR_EL1=%lx\n",
+      elr, esr, far, spsr);
   panic("Kernel instruction fetch fault detected");
 }
