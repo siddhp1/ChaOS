9ccf68d2fcca5028036da846adaa776c41e15ccd
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-05-03 14:19:06 -0400

    Update QEMU MMU map size value

--- arch/arm64/platform/qemu_virt/include/platform/mmu_map.h
+++ arch/arm64/platform/qemu_virt/include/platform/mmu_map.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define MAP_SIZE (2UL * 1024 * 1024 * 1024)  // 2 GiB
+#define MAP_SIZE 0x80000000UL
 
 static inline bool phys_is_device(uintptr_t phys) {
   if (phys < 0x40000000) return true;
