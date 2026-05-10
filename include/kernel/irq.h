8fde78baf5ede1d57067dd3ba475f938e022343c
Jenneva Li <jenli0508@gmail.com>
2026-05-10 14:55:50 -0400

    Add save and restore daif flags

--- include/kernel/irq.h
+++ include/kernel/irq.h
@@ -5,6 +5,7 @@
 
 #define IRQ_RESCHED_SGI 0
 #define IRQ_TIMER 27
+#define IRQ_UART 33
 #define IRQ_NONE 0xFFFFFFFFUL
 
 typedef void (*irq_handler_t)(void*);
@@ -16,6 +17,10 @@ void register_irq(uint32_t irq, irq_handler_t handler);
 
 void irq_disable(void);
 void irq_enable(void);
+
+uint64_t irq_save(void);
+void irq_restore(uint64_t daif);
+
 void irq_init(void);
 void irq_send_resched(void);
 
