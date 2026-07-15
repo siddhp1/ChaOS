da3859c12a6f10eaab518c4e25ac414a16dae5fd
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-07-15 19:47:57 -0400

    Add file syscall test

--- userspace/hello.c
+++ userspace/hello.c
@@ -2,5 +2,18 @@
 
 int main(void) {
   write(1, "Hello, World!\n", 14);
+
+  // Test file redirection syscalls
+  int saved = dup(1);
+  int other = open("/dev/console", 0, 0);
+
+  dup2(other, 1);
+  write(1, "test1\n", 5);
+  write(4, "test4\n", 5);
+
+  dup2(saved, 1);
+  close(saved);
+  close(other);
+
   return 0;
 }
