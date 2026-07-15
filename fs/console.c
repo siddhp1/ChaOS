bef3e80af4a7513776f31484b6e964283d7961cf
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-07-14 22:10:17 -0400

    Rewrite read backend

--- fs/console.c
+++ fs/console.c
@@ -3,12 +3,44 @@
 #include "drivers/uart.h"
 #include "fs/file.h"
 
-// long console_read(struct file* file, void* buf, size_t len) {
-//   return uart_write((const char*)buf, len);
-// }
+long console_read(struct file* file, void* buf, size_t len) {
+  (void)file;
+
+  char* kbuf = (char*)buf;
+  long count = 0;
+
+  if (!buf || len < 0) return -1;
+  if (len == 0) return 0;
+
+  while (count < len) {
+    char c;
+
+    if (uart_read(&c, 1) != 1) break;
+
+    if (c == '\r' || c == '\n') {
+      kbuf[count++] = '\n';
+      uart_write("\n", 1);
+      break;
+    }
+
+    if (c == '\b' || c == 127) {
+      if (count > 0) {
+        count--;
+        uart_write("\b \b", 3);
+      }
+      continue;
+    }
+
+    kbuf[count++] = c;
+
+    uart_write(&c, 1);
+  }
+
+  return count;
+}
 
 long console_write(struct file* file, const void* buf, size_t len) {
-(void)file;
+  (void)file;
 
   if (!buf || len < 0) return -1;
   if (len == 0) return 0;
@@ -17,7 +49,7 @@ long console_write(struct file* file, const void* buf, size_t len) {
 }
 
 static const struct file_ops console_ops = {
-    .read = NULL, .write = console_write, .close = NULL};
+    .read = console_read, .write = console_write, .close = NULL};
 
 struct file console_file = {.refcount = 1,
                             .flags = 0,
--- include/fs/console.h
+++ include/fs/console.h
@@ -3,7 +3,7 @@
 
 #include "fs/file.h"
 
-// long console_read(struct file* file, void* buf, size_t len);
+long console_read(struct file* file, void* buf, size_t len);
 long console_write(struct file* file, const void* buf, size_t len);
 
 extern struct file console_file;
