6617c9a948bae89d0d7fd854afcd8cbabdd87625
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-07-20 16:28:08 -0400

    Update toolchains

--- toolchains/aarch64-elf.mk
+++ toolchains/aarch64-elf.mk
@@ -1,4 +1,10 @@
-CROSS   = aarch64-elf-
-CC      = $(CROSS)gcc
-LD      = $(CROSS)ld
-OBJCOPY = $(CROSS)objcopy
+CROSS_COMPILE ?= aarch64-elf-
+
+CC      := $(CROSS_COMPILE)gcc
+LD      := $(CROSS_COMPILE)ld
+AS      := $(CROSS_COMPILE)as
+AR      := $(CROSS_COMPILE)ar
+NM      := $(CROSS_COMPILE)nm
+OBJCOPY := $(CROSS_COMPILE)objcopy
+OBJDUMP := $(CROSS_COMPILE)objdump
+READELF := $(CROSS_COMPILE)readelf
