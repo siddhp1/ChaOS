2c105e243cc307a00e7534a4640b4a269dac2785
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-08-26 10:45:28 -0400

    Fix minor syscall issues

--- userspace/libc/dup2.c
+++ userspace/libc/dup2.c
@@ -1,5 +1,5 @@
 #include "libc.h"
 
 long dup2(int old_fd, int new_fd) {
-  syscall(SYS_DUP2, (long)old_fd, (long)new_fd, 0, 0, 0, 0);
+  return syscall(SYS_DUP2, (long)old_fd, (long)new_fd, 0, 0, 0, 0);
 }
