e9cc2ffbfe5c23defe52c554cae48a794d15de75
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2026-08-26 18:01:07 -0400

    Define static file contents and tree

--- fs/rootfs.c
+++ fs/rootfs.c
@@ -2,6 +2,9 @@
 
 #include "fs/vfs.h"
 
+// Rootfs data
+static const char hello_data[] = "hello from vfs\n";
+
 // Rootfs-specific node type
 struct rootfs_node {
   const char* name;
@@ -12,4 +15,40 @@ struct rootfs_node {
 
   struct rootfs_node* children;
   size_t child_count;
+};
+
+static struct rootfs_node root_children[] = {
+    {
+        .name = "hello.txt",
+        .type = INODE_REG,
+        .data = hello_data,
+        .size = sizeof(hello_data) - 1,
+        .children = NULL,
+        .child_count = 0,
+    },
+    {
+        .name = "dev",
+        .type = INODE_DIR,
+        .data = NULL,
+        .size = 0,
+        .children = NULL,
+        .child_count = 0,
+    },
+    {
+        .name = "bin",
+        .type = INODE_DIR,
+        .data = NULL,
+        .size = 0,
+        .children = NULL,
+        .child_count = 0,
+    },
+};
+
+static struct rootfs_node root_node = {
+    .name = "",
+    .type = INODE_DIR,
+    .data = NULL,
+    .size = 0,
+    .children = root_children,
+    .child_count = sizeof(root_children) / sizeof(root_children[0]),
 };
